bdf9235f4c280f52d3450284df48893e21b11712
ilambrev <ilambrev@abv.bg>
2024-04-26 20:27:49 +0300

    Basic Route Controller added

---
+++ src/main/java/bg/softuni/pathfinderproject/web/RouteController.java
@@ -0,0 +1,25 @@
+package bg.softuni.pathfinderproject.web;
+
+import bg.softuni.pathfinderproject.service.RouteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/routes")
+public class RouteController {
+
+    private final RouteService routeService;
+
+    @Autowired
+    public RouteController(RouteService routeService) {
+        this.routeService = routeService;
+    }
+
+    @GetMapping
+    public String showAllRoutes() {
+
+        return "routes";
+    }
+}
